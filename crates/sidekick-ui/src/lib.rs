513df1ada7153b3a770f0e3f5e16f982c1bb394a
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-25 15:45:49 +0700

    feat: export effect brush interaction model

--- crates/sidekick-ui/src/lib.rs
+++ crates/sidekick-ui/src/lib.rs
@@ -1,5 +1,6 @@
 pub mod annotation_canvas;
 pub mod area_selector;
+pub mod effect_brush;
 pub mod native_window;
 pub mod overlay;
 pub mod settings;
@@ -9,6 +10,9 @@ pub mod window_chooser;
 
 pub use annotation_canvas::{AnnotationCanvasView, AnnotationTool, annotation_window_options};
 pub use area_selector::{AreaSelectorView, area_selector_window_options};
+pub use effect_brush::{
+    DEFAULT_EFFECT_BRUSH_RADIUS, DEFAULT_EFFECT_BRUSH_STRENGTH, EffectBrushDraft, EffectBrushKind,
+};
 pub use native_window::apply_overlay_window_behavior;
 pub use overlay::{OverlayCard, PeekTab, overlay_window_options, peek_window_options};
 pub use settings::{HotkeySettingsView, settings_window_options};
